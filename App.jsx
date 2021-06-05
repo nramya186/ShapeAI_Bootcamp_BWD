48e92677d4698b640f26788aabee1e0061ba7299
RamyaPrabhakar <52602575+nramya186@users.noreply.github.com>
2021-06-05 15:43:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Post from "./Post";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Post />
+      <Note />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="post">
+      <h1> React.js</h1>
+      <p>
+        React is an open-source front-end JavaScript library for building user
+        interfaces or UI components. It is maintained by Facebook and a
+        community of individual developers and companies. React can be used as a
+        base in the development of single-page or mobile applications.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
